560d0db1124714817eccf7111ec7cf61b2c86c25
Your Jack <bro@example.com>
2022-03-12 19:45:12 +0500

    Registration starts

---
+++ register.py
@@ -0,0 +1,11 @@
+class Register:
+    def __init__(self):
+        pass
+    def login(self):
+        pass
+    def password(self):
+        pass
+    def update_login(self):
+        pass
+    def update_password(self):
+        pass
