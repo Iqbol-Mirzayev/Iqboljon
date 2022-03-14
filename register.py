5644fb740c7bf070987e33e5ec3e031c7d768c64
Your Jack <bro@example.com>
2022-03-14 13:06:51 +0500

    nimadir

--- register.py
+++ register.py
@@ -2,10 +2,11 @@ class Register:
     def __init__(self):
         print("hello world")
     def login(self):
-        pass
+        print("hello world")
     def password(self):
         pass
     def update_login(self):
         pass
     def update_password(self):
         pass
+
