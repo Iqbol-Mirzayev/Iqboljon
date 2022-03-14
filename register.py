3ea4cbcc0c5f02e1fa810df4147ac6778a96da6a
Your Name <J1ngoo@example.com>
2022-03-14 15:12:12 +0500

    Ba'zi so'zlarni to'g'riladim, kirituvchi to'g'ri login  va password kiritmaguncha qayta sorayveradi. Omad

--- register.py
+++ register.py
@@ -1,7 +1,18 @@
+import os
+
 with open("a1.txt") as file:
     read = file.readlines()
     login = read[0].strip()
     password = read[1].strip()
-    newlog = input("welcome inter login :  ").lower().strip()
-    newpass = input("welcome inter password :  ").lower().strip()
-    
+    newlog = input("Welcome, enter your login :  ").lower().strip()
+    newpass = input("Enter your password :  ").lower().strip()
+
+
+    while newlog != login or newpass != password:
+        os.system("clear")
+        print("Login or password is wrong. Try again")
+        newlog = input("Enter your login :  ").lower().strip()
+        newpass = input("Enter your password :  ").lower().strip()
+        os.system("clear")
+    os.system("clear")
+    print("Welcome to your account!")
